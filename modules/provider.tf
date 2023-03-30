a8d7d7982287ea47ef85c806d14603381968093e
Ian <ianb@helecloud.com>
2023-03-30 15:19:08 +0100

    etcd fix

--- modules/provider.tf
+++ modules/provider.tf
@@ -1,23 +1,34 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">=2.14"
-    }
-  }
+
+    required_version = ">= 0.12.1"
+
     backend "s3" {
     bucket = "k8s-the-right-hard-way-aws"
     key    = "k8s-the-right-hard-way-aws.tfstate"
     region = "eu-west-1"
     dynamodb_table = "terraform-state-lock-dynamo"
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=2.14"
+    }
+  }
+
 }
+
+
+
 
 # Configure the AWS Provider
 provider "aws" {
     region  = var.aws_region
     profile = var.aws_profile
+
+
 }
+
+
 
   resource "aws_dynamodb_table" "state-lock" {
   name = "k8s-the-right-hard-way-aws-lock"
